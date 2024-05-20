42308605ccdce7d21a900b5feb1bda8297a02301
pixie <pixie.pixie@gmail.com>
2024-05-20 14:41:56 +0300

    initial commit

--- client.h
+++ client.h
@@ -3,6 +3,7 @@
 
 #include "network.h"
 #include <QMainWindow>
+#include <QDataStream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,6 +17,14 @@ class client : public QMainWindow
 
 public:
     client(QWidget *parent = nullptr);
+
+    // connect to server
+    // request to load one file from the list via a specific port. Ex port (7505)
+    // opportunity to choose where a file will be saved
+    // the client UI should have the ability to navigate through file system
+    // the client should have the screen divided by two. Left side is a filesystem of the client, the right side is a filesystem of the server.
+    // the file that was chosen by a user should be uploaded to the folder which is opened on the client.
+
     ~client();
 
 private:
--- network.h
+++ network.h
@@ -2,9 +2,14 @@
 #define NETWORK_H
 
 class network {
+    // Communication between client and server should be done by port 7500
+
     // ports
     // host
-    //
+    // create connection
+    // connect
+    // listen
+    // close connection
 };
 
 #endif // NETWORK_H
